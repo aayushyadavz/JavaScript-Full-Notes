6315b9378b4650163f3fa4a3c91f00c5e893a2ae
Ayush Yadav <ayushyadav24nov1234@gmail.com>
2024-04-15 15:00:02 +0530

    Updated 03 Conversion Operations JS
    Added Notes of Operations (what to use, what to not) & Added a MDN link of Increament Operators

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -4,14 +4,14 @@
 /* 1. When converting in Number,
   (i) */
 let score = "33" // "33" is a string.
-console.log(typeof score);
-console.log(typeof(score)); 
+// console.log(typeof score);
+// console.log(typeof(score)); 
 // We can write console.log in both the ways.
 
 // Converting "33" string into number,
 let valueInNumber = Number(score) // We can write String, Boolena e.t.c Data types in the place of Number.
-console.log(typeof valueInNumber); // Output : number
-console.log(valueInNumber); // Output : 33
+// console.log(typeof valueInNumber); // Output : number
+// console.log(valueInNumber); // Output : 33
 
 // (ii) 
 /* let score = "33abc"
@@ -39,8 +39,8 @@ console.log(valueInNumber); // Output : 33
  (i) */
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(typeof booleanIsLoggedIn); // Output : boolean
-console.log(booleanIsLoggedIn); // Output : true
+// console.log(typeof booleanIsLoggedIn); // Output : boolean
+// console.log(booleanIsLoggedIn); // Output : true
 
 /* (ii)
  Same in the case of 0 console.log(booleanIsLoggedIn); will return false */
@@ -61,6 +61,43 @@ console.log(booleanIsLoggedIn); // Output : True */
  (i) */
  let someNumber = 33
  let stringNumber = String(someNumber)
- console.log(typeof stringNumber); // Output : String
- console.log(stringNumber); // Output : 33
+//  console.log(typeof stringNumber); // Output : String
+//  console.log(stringNumber); // Output : 33
 
+// *************************************Operations********************************************
+
+ 
+// Operation 1,
+let value = 3
+let negValue = -value
+// console.log(negValue); // Output : -3
+
+// Other Operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // 2 to the power 2*2*2
+// console.log(2/2);
+// console.log(2%2); // For remainder
+
+// Operation 2,
+let str1 = "Hello!"
+let str2 = " Ayush"
+let str3 = str1 + str2
+// console.log(str3); // Output : Hello! Ayush
+
+// Operations which are not a good practice,
+// console.log("1" + 2); // Output : 12
+// console.log(1 + "2"); // Output : 12
+// console.log("1" + 2 + 2); // Output : 122
+// console.log(1 + 2 + "2"); // Output : 32
+// console.log(+true); // Output : 1
+// console.log(+""); // Output : 0
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+// console.log(num1, num2, num3); // Output : 4 4 4
+
+//*************************************Increament Operators***********************************
+
+// Postfix & Prefix Increament,
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
