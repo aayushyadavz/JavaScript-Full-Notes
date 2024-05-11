1c1e99661af9a8b0d3c6ee061ad5c53a830a1b89
Ayush Yadav <ayushyadav24nov1234@gmail.com>
2024-05-12 00:22:27 +0530

    Learned about "This" keyword, where "This" keyword works & also about Arrow functions and it's explicit and implicit return.

---
+++ 03_Basics/05_arrow.js
@@ -0,0 +1,60 @@
+// 5. This
+
+const user = {
+    username: "Ayush",
+    price: 999,
+    welcomeMessage: function (){
+        console.log(`${this.username}, welcome to website.`);
+        // Note: While referring current context we use 'this' keyword.
+        console.log(this); // Note: Will print the current context.
+    }
+}
+// user.welcomeMessage() // Output: Ayush, welcome to website.
+// user.username = "sam"
+// user.welcomeMessage() // Output: sam, welcome to website.
+
+console.log(this); // Output: {} Prints an empty object.
+// Note: When we run this in browser's console we get window global object.
+
+function one(){
+    console.log(this);
+}
+// one() // Output: We get many values
+
+function two() {
+    let username = "ayush"
+    console.log(this.username);
+}
+// two() // Output: Undefined
+
+const three = function (){
+    let username = "sam"
+    console.log(this.username);
+}
+// three() // Output: undefined 
+
+/* ---------------------------------------------------------------------------
+Arrow Function, */
+
+const chai = () => { // It's an arrow function
+    let name = "Ayush"
+    console.log(this.username);
+}
+chai() // Output: undefined
+// Note: If in arrow function we do console.log(this) then we get empty {}
+
+// Explicit Return
+const addTwo = (num1, num2) => {
+    return num1 + num2
+}
+console.log(addTwo(3, 4)) // Output: 7
+
+// Implicit Return,
+const add = (num1, num2) => (num1 + num2)
+console.log(add(3, 4)); // Output: 7
+
+/* Note: When using curly braces {} then we have use return keyword and when
+using parenthesis () then we don't have to use return keyword. */
+
+const returnObject = () => ({username: "ayush"})
+console.log(returnObject()) // Output: { username: 'ayush' }
