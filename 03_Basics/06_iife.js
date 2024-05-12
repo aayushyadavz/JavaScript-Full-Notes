f7989d3837c13eafb5765a22e6f9cebc1b8eb582
Ayush Yadav <ayushyadav24nov1234@gmail.com>
2024-05-12 14:47:20 +0530

    Learned about IIFE, named and simple IIFE, how to pass parameter, arguments in a IIFE function & how to write two iife in the same file.

---
+++ 03_Basics/06_iife.js
@@ -0,0 +1,16 @@
+/* 6. IIFE (Immediately Invoked Function Expressions)
+
+Note: An IIFE in JavaScript is a function that is executed immediately 
+after it's defined. It's typically used to create a private scope for variables
+and functions to avoid polluting the global namespace.
+
+Name IIFE, */
+(function chai(){
+    console.log(`DB Connected`);
+})(); /* Output: DB Connected
+Note: Using semicolon ; to end the first IIFE function. 
+
+Simple IIFE, */
+((name) /* parameter */ => {
+    console.log(`DB Connected, ${name}`);
+})("Ayush") // argument | Output: DB Connected, Ayush
