e9d37490d34eb5357ff972eb29f984626672a804
Ayush Yadav <ayushyadav24nov1234@gmail.com>
2024-06-25 22:52:19 +0530

    Async await, tryCatch, fetch

--- 10_Advance/promises.js
+++ 10_Advance/promises.js
@@ -61,3 +61,47 @@ promiseFour.then(function(user){ // Expecting to receive username
 }).finally(() => console.log('The Promise is either resolved or rejected.')) // This will always execute
 
 // -------------------------------------------------------------------------------------
+// Doing the same with Async Await
+
+const promiseFive = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let error = true
+        if (!error) { 
+            resolve({username: "deepuyadavz", password: "124"})
+        } else { 
+            reject("Something went wrong")
+        }
+    }, 1000)
+})
+
+async function consumePromiseFive () {
+    try { // To handle errors
+        const response = await promiseFive
+        console.log(response);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+consumePromiseFive() // Output : Something went wrong
+
+// -------------------------------------------------------------------------------------
+// Where do we actually use these
+
+async function getAllUsers(){
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json() /* response we'll get will be in string, 
+    so converting it into json, so that we can access values. */
+    console.log(data);
+    // We can also wrap these into tryCatch
+}
+
+getAllUsers()
+
+// With .then & .catch
+fetch('https://api.github.com/users/AayushYadavz')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {console.log(data);})
+.catch((err) => {console.log(err);})
